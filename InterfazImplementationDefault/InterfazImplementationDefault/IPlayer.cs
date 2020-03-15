44122cc5a5772b6400ab61de50f4f169b36f27ab
Magdaleno0100 <Magdaleno0100@gmail.com>
2020-03-15 02:30:29 -0600

    Implementación de métodos de interfaz

---
+++ InterfazImplementationDefault/InterfazImplementationDefault/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfazImplementationDefault
+{
+    public interface IPlayer
+    {
+        void Play()
+        {
+            Console.WriteLine("Play");
+        }
+    }
+}
---
+++ InterfazImplementationDefault/InterfazImplementationDefault/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InterfazImplementationDefault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
