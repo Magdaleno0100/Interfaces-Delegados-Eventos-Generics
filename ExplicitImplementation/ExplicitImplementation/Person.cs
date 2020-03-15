ff5f4d73d53d8883d2521e85ba63cd7045be96a0
Magdaleno0100 <Magdaleno0100@gmail.com>
2020-03-15 02:20:40 -0600

    Ejemplo de implemetanciones explicitas de interfaces

---
+++ ExplicitImplementation/ExplicitImplementation/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitImplementation
+{
+    public class Person : IPlayer, IMusician
+    {
+      void IPlayer.Play()
+        {
+            Console.WriteLine("Play video game");
+        }
+
+        void IMusician.Play()
+        {
+            Console.WriteLine("Play guitar");
+        }
+        public void Play()
+        {
+            Console.WriteLine("Método de clase");
+        }
+
+    }
+}
---
+++ ExplicitImplementation/ExplicitImplementation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExplicitImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            var player = person as IPlayer;
+            var musician = person as IMusician;
+
+            person.Play();
+            player.Play();
+            musician.Play();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
